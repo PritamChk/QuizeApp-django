73b441d8975290f2bc66b424d3cea2ce3b08bccc
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-14 00:16:49 +0530

    teacher , classroom , student working fine

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,16 +1,30 @@
+from pyexpat import model
 from django.contrib import admin
 
 from .models import *
 
+admin.site.site_header= "Quizapp Admin"
+admin.site.site_title= "Admin Section"
+
+# -------------  inline classes -------------
+# class StudentInline(admin.TabularInline):
+#     model = Student
+#     raw_id_fields = ("classroom",)
+#     min_num = 0
+#     max_num = 10
+#     extra = 0
+
+# -------------  inline classes -------------
 
 # ---------------- CLASSROOM ADMIN --------------
 @admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
+    # inlines = [StudentInline]
     list_display = [
         "title",
         "subject",
         "count_teachers",
-        "count_students"            
+        "count_students",
     ]
     search_fields = ["title__icontains",
                      "title__istartswith",
@@ -31,6 +45,14 @@ class TeacherAdmin(admin.ModelAdmin):
         "count_classroom"
     ]
     readonly_fields = ["last_updated","joined_at"]
+    search_fields = [
+        "first_name__icontains",
+        "first_name__istartswith",
+        "first_name__iendswith",
+        "last_name__icontains",
+        "last_name__istartswith",
+        "last_name__iendswith",
+    ]
     ordering = ["first_name","last_name"]
     autocomplete_fields = ["classroom"]
     list_per_page = 10
@@ -43,6 +65,7 @@ class TeacherAdmin(admin.ModelAdmin):
 
 # --------------- STUDENT ADMIN -----------------
 
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     autocomplete_fields = ["classroom"]
@@ -53,4 +76,12 @@ class StudentAdmin(admin.ModelAdmin):
         ]
     ordering = ["first_name","last_name"]
     readonly_fields = ["last_updated","joined_at"]
+    search_fields = [
+        "first_name__icontains",
+        "first_name__istartswith",
+        "first_name__iendsswith",
+        "last_name__icontains",
+        "last_name__istartswith",
+        "last_name__iendswith",
+    ]
 # --------------- STUDENT ADMIN -----------------    

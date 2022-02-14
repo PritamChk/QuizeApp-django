9ac5a5c569a94eab24ee97ab5fb8ccd33d204b1d
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-14 13:29:33 +0530

    quizset, questions , options model added

--- classroom/models.py
+++ classroom/models.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from django.core.validators import MaxValueValidator
 from django.db.models import (
     Model,
     CharField,
@@ -8,9 +9,12 @@ from django.db.models import (
     EmailField,
     PositiveSmallIntegerField,
     TextField,
+    BooleanField,
     ForeignKey,
     OneToOneField,
-    ManyToManyField
+    ManyToManyField,
+    CASCADE,
+    SET_NULL,
 )
 
 STR_MAX_LEN = 300
@@ -38,17 +42,18 @@ class BaseUser(Model):
     password = CharField(max_length=STR_MAX_LEN)
     joined_at = DateTimeField(auto_now_add=True, editable=False)
     last_updated = DateTimeField(auto_now=True, editable=False)
-    
+
     class Meta:
         abstract = True
-    
+        ordering = ["first_name", "last_name", "-joined_at"]
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
 
 class Teacher(BaseUser):
     classroom = ManyToManyField(
         "Classroom", related_name="teachers", blank=True)
-
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
 
     def count_classroom(self):
         count_class = self.classroom.count()
@@ -58,12 +63,60 @@ class Teacher(BaseUser):
 class Student(BaseUser):
     classroom = ManyToManyField(Classroom, related_name="students", blank=True)
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
-
     def count_classroom(self):
         count_class = self.classroom.count()
         return count_class
+
+
+class Question(Model):
+    qustion_value = TextField()
+    point = PositiveSmallIntegerField(default=1, validators=[MaxValueValidator(
+        15, "Hya hya, 1ta mcq tei 100 marks diye dao,Ajob Public")])
+    updated_at = DateTimeField(auto_now=True)
+    quizset = ForeignKey("QuizSet", on_delete=CASCADE, related_name="qustions")
+    #options = ...
+
+    class Meta:
+        ordering = ["point"]
+
+    def __str__(self) -> str:
+        return self.question_value.lower()[:20]
+
+
+class Options(Model):
+    option_value = TextField()
+    is_correct = BooleanField(default=False, editable=True)
+    qustion = ForeignKey(Question, on_delete=CASCADE, related_name="options")
+
+    def __str__(self) -> str:
+        return self.option_value.lower()[:15]
+
+
+class QuizSet(Model):
+    LEVEL_EASY = 0
+    LEVEL_MEDIUM = 1
+    LEVEL_HARD = 2
+    LEVEL = [
+        (LEVEL_EASY, "Easy"),
+        (LEVEL_MEDIUM, "Medium"),
+        (LEVEL_HARD, "Hard"),
+    ]
+
+    heading = CharField(max_length=STR_MAX_LEN,
+                        default=uuid4, db_index=True, blank=True)
+    created_at = DateTimeField(auto_now_add=True, db_index=True)
+    update_at = DateTimeField(auto_now=True, editable=False)
+    dificulty_level = CharField(
+        max_length=2, choices=LEVEL, default=LEVEL_EASY)
+    author_teacher = ForeignKey(
+        Teacher, on_delete=SET_NULL, related_name='quizsets')
+    #questions = ...
+
+    class Meta:
+        ordering = ["-created_at", "dificulty_level"]
+
+    def __str__(self) -> str:
+        return f"{self.id} - {self.heading[:25]}"
+
+    def get_teacher_count_for_each_quizset(self):
+        return self.author_teacher.count()
